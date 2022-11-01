aca1d1695b88f113f088e7ffca81ee66cca0ef7a
Craig Doremus <cdoremus@gmail.com>
2022-11-01 11:19:42 -0400

    Removed deletion confirmation

--- islands/Todo.tsx
+++ islands/Todo.tsx
@@ -9,47 +9,15 @@ type TodoProps = {
 
 export default function Todo({text, index}: TodoProps) {
   const { removeTodo } = useContext(AppState);
-  const showConfirm = useSignal<boolean>(false);
-
-
-  const handleRemove = () => {
-    // if (confirm("Do you really want to delete this item?")) {
-    //   removeTodo(index)
-    // }
-    showConfirm.value = true;
-  }
-
-  const dialogYes = () => {
-    showConfirm.value = false;
-    removeTodo(index);
-  }
-
-  const dialogNo = () => {
-    showConfirm.value = false;
-  }
-
-  const confirmDelete = () => {
-    return (
-      <div id="confirmDialog" className="confirm">
-        <div>Are you sure you want to delete?</div>
-        <button onClick={dialogYes}>Yes</button>
-        <button onClick={dialogNo}>No</button>
-      </div>
-    );
-  }
 
   return (
     <div className="list-item" key={text}>
       <div className="item-todo">
         {text}
       </div>
-      <button className="x-button" onClick={handleRemove}>
+      <button className="x-button" onClick={() => removeTodo(index)}>
         X
       </button>
-      { showConfirm.value ?
-        confirmDelete()
-        : ""
-      }
     </div>
 
   )
