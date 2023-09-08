e00c011b465dc583a73f1414dcd54f0e1f9eb237
Craig Doremus <cdoremus@gmail.com>
2023-09-08 19:32:08 -0400

    fixed AddTodo component tests

--- tests/AddTodo_test.tsx
+++ tests/AddTodo_test.tsx
@@ -4,24 +4,28 @@ import { cleanup, fireEvent, render, setup } from "$fresh-testing-library";
 import { afterEach, beforeAll, describe, it } from "$std/testing/bdd.ts";
 import { createContext } from "preact";
 import state, { type AppStateType } from "../state.ts";
+import { AppState } from "../islands/App.tsx";
 import AddTodo from "../islands/AddTodo.tsx"
-
-// create the AppState context
-const AppState = createContext<AppStateType>({} as AppStateType);
 
 describe("AddTodo.tsx test", () => {
   beforeAll(setup);
   afterEach(cleanup);
+
   it("should add todo...", () => {
+    const todo = "Current todo1";
+    state.currentTodo.value = todo;
     const screen = render(
       <AppState.Provider value={state}>
         <AddTodo />
       </AppState.Provider>
     );
     const input = screen.getByLabelText("Add todo:");
-    // console.log("input ", input);
-    const newTodo = "First todo";
-    fireEvent.change(input, { value: newTodo });
-    assertEquals(state.currentTodo.value, newTodo);
+    const val = (input as HTMLInputElement).value;
+    // assert current todo value is input value
+    assertEquals(val, todo);
+    const newVal = "New value";
+    (input as HTMLInputElement).value = newVal;
+    fireEvent.change(input);
+    assertEquals(state.currentTodo.value, newVal);
   })
 });
