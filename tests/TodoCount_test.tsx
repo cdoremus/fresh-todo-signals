924e5660204440062d9230bbdbb576ecae2bfbd6
Craig Doremus <cdoremus@gmail.com>
2023-09-09 12:11:21 -0400

    Test for TodoCount component

---
+++ tests/TodoCount_test.tsx
@@ -0,0 +1,25 @@
+import { Signal, signal } from "@preact/signals";
+import { assert, assertEquals, assertExists, assertFalse, assertStringIncludes } from "$std/testing/asserts.ts";
+import { cleanup, fireEvent, render, setup } from "$fresh-testing-library";
+import { afterEach, beforeAll, describe, it } from "$std/testing/bdd.ts";
+import { createContext } from "preact";
+import state, { type AppStateType } from "../state.ts";
+import { AppState } from "../islands/App.tsx";
+import TodoCount from "../islands/TodoCount.tsx";
+
+describe("TodoCount.tsx test", () => {
+  beforeAll(setup);
+  afterEach(cleanup);
+
+  it("should display number of todos...", () => {
+    const todos = ["Foo", "Bar", "Baz"];
+    state.todos.value = todos;
+    const screen = render(
+      <AppState.Provider value={state}>
+        <TodoCount/>
+      </AppState.Provider>
+    );
+    const countElement = screen.getByText("Todo Count: " + todos.length);
+    assertEquals("Todo Count: 3", countElement.textContent);
+  });
+});
