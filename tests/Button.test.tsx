417e3ee2c13345339a3b71ea578f01b3ab56ce3d
Craig Doremus <cdoremus@gmail.com>
2023-10-11 12:03:36 -0400

    Removed unnecessary assertExists calls

--- tests/Button.test.tsx
+++ tests/Button.test.tsx
@@ -1,4 +1,4 @@
-import { assert, assertExists } from "$std/assert/mod.ts";
+import { assert } from "$std/assert/mod.ts";
 import { cleanup, render, setup } from "$fresh-testing-library";
 import { afterEach, beforeAll, describe, it } from "$std/testing/bdd.ts";
 import { Button } from "../components/Button.tsx";
@@ -10,28 +10,24 @@ describe("Button.tsx test", () => {
   it("should be disabled...", () => {
     const {getByRole} = render(<Button disabled/>);
     const textElement = getByRole("button");
-    assertExists(textElement);
     assert((textElement as HTMLButtonElement).disabled === true)
   });
 
   it("should not be disabled...", () => {
     const { getByRole } = render(<Button/>);
     const textElement = getByRole("button");
-    assertExists(textElement);
     assert((textElement as HTMLButtonElement).disabled === false)
   });
 
   it("should contain touch action style", () => {
     const { getByRole } = render(<Button disabled/>);
     const textElement = getByRole("button");
-    assertExists(textElement);
     assert((textElement as HTMLButtonElement).style.touchAction === 'manipulation');
   });
 
   it("should contain border class", () => {
     const { getByRole } = render(<Button disabled/>);
     const textElement = getByRole("button");
-    assertExists(textElement);
     assert((textElement as HTMLButtonElement).classList.contains("border"));
   });
 
